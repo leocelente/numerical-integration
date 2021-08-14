d16d600890b6332994ecf416aca5d06b68a79779
Leonardo Celente <leocelente@gmail.com>
2021-08-14 03:42:28 -0300

    Main array of structs is now on the heap

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,21 @@
 #include <algorithm>
 #include <array>
 #include <numeric>
-
+#include <memory>
 #include "module.hpp"
 
 
 int main(int const argc, char *argv[]) {
   constexpr auto start = 0.f;
   constexpr auto end = 5.f;
-	constexpr auto dt = 0.1f;
+	constexpr auto dt = 0.01f;
 	constexpr auto N = static_cast<int>((end - start) / dt);
 
-  std::array<Step<float>, N> data{};
-	std::generate(
-	  data.begin(), data.end(), [dt, s = Step<float>{}]() mutable {
+  // std::array<Step<float>, N> data{};
+	auto data = std::make_unique<std::array<Step<float>, N>>();
+
+  std::generate(
+	  data->begin(), data->end(), [dt, s = Step<float>{}]() mutable {
 		  s.time += dt;
 		  return s;
 	  });
@@ -23,9 +25,9 @@ int main(int const argc, char *argv[]) {
 		return delta;
 	};
 
-	integrate(df, data.begin(), data.end(), dt);
+	integrate(df, data->begin(), data->end(), dt);
 
-	std::for_each(data.begin(), data.end(), [](auto step) {
+	std::for_each(data->begin(), data->end(), [](auto step) {
 		step.print();
 		fmt::print("\n");
 	});
