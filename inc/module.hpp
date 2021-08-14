9184e064f0f8971e24dad5e073934e2efd71b9a5
Leonardo Celente <leocelente@gmail.com>
2021-08-14 00:59:07 -0300

    Library implementation with naive integration

--- inc/module.hpp
+++ inc/module.hpp
@@ -1,9 +1,50 @@
 #ifndef MODULE_HPP
 #define MODULE_HPP
 
-/*
-	Delete before use
-*/
-bool test();
+#include <algorithm>
+#include <array>
+#include <execution>
+#include <fmt/ranges.h>
+#include <functional>
+#include <numeric>
+#include <string>
+
+
+template<class T, int N> using State = std::array<T, N>;
+template<class T, int N> using View = std::array<T, N>;
+
+template<class T> struct Step {
+	State<T, 1> state{};
+	View<T, 1> view{};
+
+	void print() {
+		fmt::print("{}", fmt::join(state, ","));
+		fmt::print(",{}", fmt::join(view, ","));
+	}
+};
+
+template<class T>
+Step<T> operator+(Step<T> const &l, Step<T> const &r) noexcept {
+	Step<T> c{};
+	std::transform(std::execution::par_unseq,
+	  l.state.cbegin(),
+	  l.state.cend(),
+	  r.state.cbegin(),
+	  c.state.begin(),
+	  std::plus<T>());
+	return c;
+}
+
+template<class ForwardIt, class OutputIt, class Function>
+void integrate(Function f, ForwardIt first, ForwardIt last, OutputIt o_first) {
+	std::accumulate(
+	  first, last, Step<float>{}, [&f, &o_first](auto state_prev, auto t) {
+		  auto next = state_prev + f(t);
+		  *o_first = next;
+		  o_first++;
+		  return next;
+	  });
+}
+
 
 #endif
