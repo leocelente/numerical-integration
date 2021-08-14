f56458681cdfb88646daac2d6b5567afd33093d5
Leonardo Celente <leocelente@gmail.com>
2021-08-14 00:59:51 -0300

    Example of API integrating 2*x

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,27 @@
-#include "module.hpp"
-#include <ctre.hpp>
-#include <fmt/core.h>
-#include <spdlog/spdlog.h>
+#include <algorithm>
+#include <array>
+#include <numeric>
 
+#include "module.hpp"
 
 int main(int const argc, char *argv[]) {
+	std::array<float, 100000> time{};
+	std::array<Step<float>, time.size()> data{};
+	std::generate(time.begin(), time.end(), [x = 0.f]() { return x + .1f; });
 
-	if (test()) fmt::print("Hello There!\n");
+
+	auto df = [](auto t) {
+		Step<float> delta = { { 2.f * t }, { 0.f } };
+		return delta;
+	};
+
+	integrate(df, time.cbegin(), time.cend(), data.begin());
+
+	std::for_each(data.begin(), data.end(), [](auto step) {
+		step.print();
+		fmt::print("\n");
+	});
+
+
 	return 0;
 }
