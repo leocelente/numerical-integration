b3611d477486d8c427ac2c298af5e6dadc7be933
Leonardo Celente <leocelente@gmail.com>
2021-08-14 03:04:37 -0300

    Joined time variable in the same array of structs, also fixed formula

--- inc/module.hpp
+++ inc/module.hpp
@@ -14,10 +14,12 @@ template<class T, int N> using State = std::array<T, N>;
 template<class T, int N> using View = std::array<T, N>;
 
 template<class T> struct Step {
+	T time;
 	State<T, 1> state{};
 	View<T, 1> view{};
 
 	void print() {
+		fmt::print("{},", time);
 		fmt::print("{}", fmt::join(state, ","));
 		fmt::print(",{}", fmt::join(view, ","));
 	}
@@ -25,7 +27,7 @@ template<class T> struct Step {
 
 template<class T>
 Step<T> operator+(Step<T> const &l, Step<T> const &r) noexcept {
-	Step<T> c{};
+	Step<T> c{ r };
 	std::transform(std::execution::par_unseq,
 	  l.state.cbegin(),
 	  l.state.cend(),
@@ -35,13 +37,28 @@ Step<T> operator+(Step<T> const &l, Step<T> const &r) noexcept {
 	return c;
 }
 
-template<class ForwardIt, class OutputIt, class Function>
-void integrate(Function f, ForwardIt first, ForwardIt last, OutputIt o_first) {
-	std::accumulate(
-	  first, last, Step<float>{}, [&f, &o_first](auto state_prev, auto t) {
-		  auto next = state_prev + f(t);
-		  *o_first = next;
-		  o_first++;
+template<class T> Step<T> operator*(T a, Step<T> const &r) noexcept {
+	auto y = r;
+	std::transform(
+	  r.state.cbegin(), r.state.cend(), y.state.begin(), [&a](auto s) {
+		  return a * s;
+	  });
+	return y;
+}
+
+template<class T> Step<T> operator*(Step<T> const &l, T a) noexcept {
+	return a * l;
+}
+
+
+template<class T, class ForwardIt, class Function>
+void integrate(Function f, ForwardIt first, ForwardIt last, T const dt) {
+	std::accumulate(first,
+	  last,
+	  Step<T>{},
+	  [&f, &dt, i = first](auto prev, auto step) mutable {
+		  auto next = prev + dt * f(step.time);
+		  *i++ = next;
 		  return next;
 	  });
 }
